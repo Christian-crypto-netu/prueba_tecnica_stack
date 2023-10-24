1e1958e755105efacbca9dab7703ce64d9e71390
Cristian Cardenas <ccamilocar@hotmail.com>
2023-10-24 17:43:20 -0500

    componentes agregados en el frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'registrar', component: RegistrarComponent },
+    { path: 'tabla', component: TablaComponent },
+    { path: '**', component: InicioComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
